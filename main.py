3c3966730bc98e217ea08a24f8798914d3858fef
Breeoo1e <warigiabrendah1@gmail.com>
2025-04-18 22:40:29 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from pet import Pet
+
+def main():
+    my_pet = Pet("Buddy")
+
+    my_pet.get_status()
+
+    my_pet.eat()
+    my_pet.sleep()
+    my_pet.play()
+    my_pet.train("roll over")
+    my_pet.train("sit")
+
+    my_pet.get_status()
+    my_pet.show_tricks()
+
+if __name__ == "__main__":
+    main()
